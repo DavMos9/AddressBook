93cd5605eb1a42801090468122d05d9ba90af9d7
DavMos9 <d.moscariello9@gmail.com>
2024-12-05 11:34:03 +0100

    Coommenti doxygen Classe App e Classe GestoreFile

--- AddressBook/src/main/java/com/mycompany/addressbook/App.java
+++ AddressBook/src/main/java/com/mycompany/addressbook/App.java
@@ -7,10 +7,30 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
+
+/**
+ * @class App
+ * @brief Classe principale per l'applicazione rubrica telefonica.
+ *
+ * Questa classe gestisce l'avvio dell'applicazione e il caricamento dell'interfaccia utente.
+ *
+ * @author Moscariello
+ *
+ */
+
 public class App extends Application {
 
     private static Scene scene;
 
+    /**
+     * @brief Avvia l'applicazione.
+     *
+     * Questo metodo viene chiamato automaticamente all'avvio dell'applicazione.
+     * Imposta la scena principale definita nel file FXML.
+     *
+     * @param stage La scena principale dell'applicazione.
+     * @throws IOException Se il file FXML non può essere caricato.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("Progetto"), 1000, 800);
@@ -18,14 +38,44 @@ public class App extends Application {
         stage.show();
     }
 
+    /**
+     * @brief Imposta la configurazione radice della scena corrente.
+     *
+     * Questo metodo consente di cambiare la configurazione principale dell'applicazione
+     * caricando un nuovo file FXML.
+     *
+     * @param fxml Il nome del file FXML da caricare.
+     * @throws IOException Se il file FXML non può essere caricato.
+     */
+
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
+
+
+    /**
+     * @brief Carica un file FXML e restituisce il nodo radice.
+     *
+     * Questo metodo utilizza FXMLLoader per caricare un file FXML e restituire
+     * la configurazione radice definito in esso.
+     *
+     * @param fxml Il nome del file FXML da caricare.
+     * @return Il nodo radice del layout caricato.
+     * @throws IOException Se il file FXML non può essere caricato.
+     */
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
+
+    /**
+     * @brief Punto di ingresso principale dell'applicazione.
+     *
+     * Questo metodo avvia l'applicazione JavaFX.
+     *
+     * @param args Argomenti della riga di comando.
+     */
 
     public static void main(String[] args) {
         launch();
--- AddressBook/src/main/java/com/mycompany/addressbook/GestoreFile.java
+++ AddressBook/src/main/java/com/mycompany/addressbook/GestoreFile.java
@@ -6,14 +6,42 @@
 package com.mycompany.addressbook;
 
 /**
+ * @class GestoreFile
+ * @brief Classe per la gestione del file di salvataggio della rubrica.
  *
- * @author david
+ * Questa classe fornisce metodi per leggere i dati da un file e scriverli
+ * in un file,gestendo così il caricamento e il salvataggio della rubrica.
+ *
+ * @author Moscariello
+ *
  */
 public class GestoreFile {
-    
+
+    /**
+     * @brief Legge i dati della rubrica da un file.
+     *
+     * Questo metodo apre il file specificato, legge i dati e li converte
+     * in un'istanza di `InterfacciaRubrica`.
+     *
+     * @param nomefile Il nome del file da cui leggere i dati.
+     * @return Un'istanza di `InterfacciaRubrica` contenente i dati letti.
+     *         Se il file non esiste restituisce un errore.
+     *
+     */
+
     public static InterfacciaRubrica leggi(String nomefile){
         return null;
     }
+
+    /**
+     * @brief Scrive i dati della rubrica in un file.
+     *
+     * Questo metodo salva i dati della rubrica corrente nel file specificato.
+     * Se il file non esiste, viene creato.
+     *
+     * @param nomefile Il nome del file in cui scrivere i dati.
+     */
+
     public void scrivi(String nomefile){
         
     }
