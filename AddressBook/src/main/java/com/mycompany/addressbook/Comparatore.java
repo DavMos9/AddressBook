0cab58603cea2b0436e6134b7fca964c2b3ca983
Domenico <sessa.mimmo@gmail.com>
2024-12-06 09:53:17 +0100

    Inseriti i commenti per Doxygen

--- AddressBook/src/main/java/com/mycompany/addressbook/Comparatore.java
+++ AddressBook/src/main/java/com/mycompany/addressbook/Comparatore.java
@@ -7,11 +7,32 @@ package com.mycompany.addressbook;
 
 import java.util.Comparator;
 
+
 /**
+ * @class Comparatore
  *
- * @author david
+ * @brief Classe di riferimento per l'ordinamento della rubrica telefonica.
+ *
+ * La classe Comparatore permette di confrontare due contatti in modo tale che si
+ * possano ordinare in base al cognome e nome.
+ *
+ *  @author Domenico
  */
 public class Comparatore implements Comparator<Contatto>{ //rispetto il principio di buona progettazione
+
+    /**
+     * @brief Connfronto dei contatti
+     *
+     * Questo Metodo riceve in input due parametri che corrispondono ai dati di due contatti
+     * i contatti in ingresso sono di tipo Contatto e sono idenfificati come c1 e c2
+     * e restituisce un valore numerico (0 oppure 1) rispettivamente se il primo contatto risulta
+     * essere minore o maggiore del secondo contatto
+     *
+     * @param c1 rappresenta il primo contatto da confrontare
+     * @param c2 rappresenta il secondo contatto da confrontare
+     * @return restituisce il valore che indica l'ordine dei contatti inseriti
+     */
+
     @Override
     public int compare(Contatto c1, Contatto c2) {
 
--- AddressBook/src/main/java/com/mycompany/addressbook/InterfacciaRubrica.java
+++ AddressBook/src/main/java/com/mycompany/addressbook/InterfacciaRubrica.java
@@ -9,12 +9,60 @@ import java.util.List;
 
 /**
  *
- * @author david
+ * @interface InterfacciaRubrica
+ *
+ * @brief Classe di front-end per la gestione delle principali funzionalità della rubrica.
+ *
+ * La classe InterfacciaRubrica definisce le seguenti operazioni sulla rubrica telefonica:
+ * aggiungiContatto, modificaContatto, eliminaContatto, ricercaContatto.
+ *
+ * @author Domenico
  */
 public interface InterfacciaRubrica {
+
+    /**
+     * @brief
+     * aggiungiContatto: accetta in input un parametro di tipo Contatto
+     *       Permette l'aggiunta di un nuovo contatto nella rubrica
+     *
+     *
+     * @param c c rappresenta il contatto da inserire
+     */
     void aggiungiContatto(Contatto c);
+
+    /**
+     * @brief
+     * modificaContatto: accetta in input un parametro di tipo Contatto
+     *       Permette la modifica dei valori dei campi di un contatto presente nella rubrica
+     *
+     * @param c c rappresenta il contatto su cui effettuare le modifiche
+     */
     void modificaContatto(Contatto c);
+
+    /**
+     *
+     * eliminaContatto: accetta in input un parametro di tipo Contatto
+     *       Permette di eliminare (rimozione fisica) un contatto presente mella rubrica
+     *
+     * @param c c rappresenta il contatto da eliminare
+     */
     void eliminaContatto(Contatto c);
+
+
+    /**
+     * @brief
+     *
+     * ricercaContatto: accetta in input un parametro di tipo Contatto e restituisce una lista
+     *      Permette di ricercare nella rubrica tutti i contatti il cui nome o cognome
+     *      inizia con la stringa inserita nella ricerca.
+     *      L’operazione di ricerca restituisce l'elenco dei contatti che soddisfa il criterio
+     *      di ricerca.
+     *
+     *
+     *
+     * @param c c rappresenta il contatto da ricercare in rubrica
+     * @return  viene restituita una lista contenente tutti i contatti che soddisfano i criteri di ricerca
+     */
     Contatto ricercaContatto(Contatto c);
     List<Contatto> getLista();
 
