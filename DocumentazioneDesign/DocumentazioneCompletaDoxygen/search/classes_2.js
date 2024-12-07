6da70713a5d38cea2eee194dda012670a001ce55
Davide Quaranta <d.quaranta8@studenti.unisa.it>
2024-12-07 19:29:15 +0100

    Documentazione Completa Doxygen

---
+++ DocumentazioneDesign/DocumentazioneCompletaDoxygen/search/classes_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gestorefile_0',['GestoreFile',['../classcom_1_1mycompany_1_1addressbook_1_1gestionerubrica_1_1_gestore_file.html',1,'com::mycompany::addressbook::gestionerubrica']]]
+];
---
+++ DocumentazioneDesign/DocumentazioneCompletaDoxygen/search/functions_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['ricercaaction_0',['ricercaAction',['../classcom_1_1mycompany_1_1addressbook_1_1controllers_1_1_rubrica_controller.html#a52431e39867bec9ef986bd2fc438f442',1,'com::mycompany::addressbook::controllers::RubricaController']]],
+  ['ricercacontatto_1',['ricercaContatto',['../interfacecom_1_1mycompany_1_1addressbook_1_1rubrica_1_1_interfaccia_rubrica.html#a59d282faf33f59b259ee180c0d81a275',1,'com.mycompany.addressbook.rubrica.InterfacciaRubrica.ricercaContatto()'],['../classcom_1_1mycompany_1_1addressbook_1_1rubrica_1_1_rubrica.html#a4c64cdd36e56bc77b7627a802c0154f2',1,'com.mycompany.addressbook.rubrica.Rubrica.ricercaContatto(Contatto c)']]],
+  ['rubrica_2',['Rubrica',['../classcom_1_1mycompany_1_1addressbook_1_1rubrica_1_1_rubrica.html#a4104e5a9df84b0b6059871bb83faa7da',1,'com::mycompany::addressbook::rubrica::Rubrica']]]
+];
